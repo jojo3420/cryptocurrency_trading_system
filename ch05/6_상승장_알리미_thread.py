12c3ea187478c84303dd0a21a82befc4ba2c0ae0
jojo3420 <jjjhhhvvv@gmail.com>
2021-06-14 13:14:37 +0900

    5장 상승장 알리미 구현(추가)

--- ch05/6_상승장_알리미_thread.py
+++ ch05/6_상승장_알리미_thread.py
@@ -10,52 +10,54 @@ from pandas import DataFrame
 tickers = ["BTC", "ETH", "BCH", "ETC"]
 
 
-# 사용자 정의 쓰레드
+def get_market_info(ticker: str, days: int = 5) -> tuple:
+    try:
+        is_bull = '하락장'
+        df = moving_average_by(ticker, days)
+        MA = df['close'].rolling(window=days).mean()
+        current_price = Bithumb.get_current_price(ticker)
+        prev_ma = MA[-2]
+        if current_price > prev_ma:
+            is_bull = '상승장'
+
+        return current_price, prev_ma, is_bull
+    except Exception as e:
+        print(str(e))
+        traceback.print_exc()
+        return None, None, None
+
+
+def moving_average_by(ticker: str, days: int = 5) -> DataFrame:
+    try:
+        prices: DataFrame = Bithumb.get_candlestick(ticker)
+        close = prices['close']
+        MA = close.rolling(days).mean()
+        prices[f'MA{days}'] = MA
+        prices = prices.dropna()
+        return prices
+    except Exception as e:
+        print(str(e))
+        traceback.print_exc()
+
+
+# 사용자 정의 쓰레드 class
 class Worker(QThread):
     # 사용자 정의 시그널 정의
-    finished = pyqtSignal(dict)
+    fetch_finished = pyqtSignal(dict)
 
     # 오버라이딩
     def run(self):
         while True:
             data = {}
             for ticker in tickers:
-                ma3_data: set = self.get_market_info(ticker, 3)
-                ma5_data: set = self.get_market_info(ticker, 5)
+                ma3_data: set = get_market_info(ticker, 3)
+                ma5_data: set = get_market_info(ticker, 5)
                 # print(ma5_data)
                 data[ticker] = ma3_data + ma5_data[1:]
 
             # print(data)
-            self.finished.emit(data)  # emit(event) 발생
+            self.fetch_finished.emit(data)  # emit(event) 발생
             self.msleep(500)  # 0.5초
-
-    def get_market_info(self, ticker: str, days: int = 5) -> tuple:
-        try:
-            is_bull = '하락장'
-            df = self.moving_average_by(ticker, days)
-            MA = df['close'].rolling(window=days).mean()
-            current_price = Bithumb.get_current_price(ticker)
-            prev_ma = MA[-2]
-            if current_price > prev_ma:
-                is_bull = '상승장'
-
-            return current_price, prev_ma, is_bull
-        except Exception as e:
-            print(str(e))
-            traceback.print_exc()
-            return None, None, None
-
-    def moving_average_by(self, ticker: str, days: int = 5) -> DataFrame:
-        try:
-            prices: DataFrame = Bithumb.get_candlestick(ticker)
-            close = prices['close']
-            MA = close.rolling(days).mean()
-            prices[f'MA{days}'] = MA
-            prices = prices.dropna()
-            return prices
-        except Exception as e:
-            print(str(e))
-            traceback.print_exc()
 
 
 form_class = uic.loadUiType("bull.ui")[0]
@@ -69,15 +71,19 @@ class MyWindow(QMainWindow, form_class):
         self.setWindowTitle('상승장 알리미')
 
         self.worker = Worker()
-        self.worker.finished.connect(self.update_table_widget)
+        self.worker.fetch_finished.connect(self.update_table_widget)
         self.worker.start()
 
     @pyqtSlot(dict)
     def update_table_widget(self, data: dict):
-        print('인터벌')
+        print('refresh')
         try:
             for i, ticker in enumerate(tickers):
+
+                # get data
                 price, ma3, is_bull_3, ma5, is_bull_5 = data[ticker]
+
+                # create widget item
                 ticker_item = QTableWidgetItem(ticker)
                 price_item = QTableWidgetItem(f'{int(price):,}')
                 ma3_item = QTableWidgetItem(f'{int(ma3):,}')
@@ -85,6 +91,7 @@ class MyWindow(QMainWindow, form_class):
                 ma5_item = QTableWidgetItem(f'{int(ma5):,}')
                 bull_item_5 = QTableWidgetItem(is_bull_5)
 
+                # set(행, 열, item)
                 self.tableWidget.setItem(i, 0, ticker_item)
                 self.tableWidget.setItem(i, 1, price_item)
                 self.tableWidget.setItem(i, 2, ma3_item)
