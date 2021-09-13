4aff69a4fcddb2c64a8a454dfe6ab4de1ac4bc89
jojo3420 <jjjhhhvvv@gmail.com>
2021-09-13 20:41:20 +0900

    upbit 변동성 돌파 전략 시작

---
+++ trader_bot/math_helper.py
@@ -0,0 +1,5 @@
+from common.utils import calc_williams_R, calc_moving_average_by, calc_fix_moving_average_by
+
+
+
+
---
+++ trader_bot/upbit_helper.py
@@ -0,0 +1,14 @@
+def buy(ticker: str, qty: float):
+    pass
+
+
+def sell(ticker: str, qty: float):
+    pass
+
+
+def get_balance_by(ticker="ALL"):
+    pass
+
+
+def get_transaction_history():
+    pass
